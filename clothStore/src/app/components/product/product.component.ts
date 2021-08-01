3e7ef1c13dd1147c0b06f29c5e7d67049f44d6d7
wassef911 <1.wassef911@gmail.com>
2021-08-01 23:45:28 +0100

    finished product component

--- clothStore/src/app/components/product/product.component.ts
+++ clothStore/src/app/components/product/product.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+import { map } from "rxjs/operators";
+declare let $: any;
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.sass']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, AfterViewInit {
+  id: number;
+  product: any;
+  thumbimages: any[] = [];
+  @ViewChild('quantity') quantityInput: any;
 
-  constructor() { }
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map((param: ParamMap) => {
+        // @ts-ignore
+        return param.params.id;
+      })
+    ).subscribe(prodId => {
+      this.id = prodId;
+      this.productService.getSingleProduct(this.id).subscribe(prod => {
+        this.product = prod;
+        if (prod.images !== null) {
+          this.thumbimages = prod.images.split(';');
+        }
+
+      });
+    });
   }
 
+  ngAfterViewInit(): void {
+    $('#product-main-img').slick({
+      infinite: true,
+      speed: 300,
+      dots: false,
+      arrows: true,
+      fade: true,
+      asNavFor: '#product-imgs',
+    });
+
+    $('#product-imgs').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      centerMode: true,
+      focusOnSelect: true,
+      centerPadding: 0,
+      vertical: true,
+      asNavFor: '#product-main-img',
+      responsive: [{
+        breakpoint: 991,
+        settings: {
+          vertical: false,
+          arrows: false,
+          dots: true,
+        }
+      },
+      ]
+    });
+  }
+
+
+  addToCart(id: number) {
+    this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
+  }
+
+  Increase() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity >= 1) {
+      value++;
+
+      if (value > this.product.quantity) {
+        // @ts-ignore
+        value = this.product.quantity;
+      }
+    } else {
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  Decrease() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity > 0) {
+      value--;
+
+      if (value <= 0) {
+        // @ts-ignore
+        value = 0;
+      }
+    } else {
+      return;
+    }
+    this.quantityInput.nativeElement.value = value.toString();
+  }
 }
